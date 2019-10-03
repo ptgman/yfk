3957971f51e0e6d9b6cd5af8b02366b9322073df
PTGM <develop@ptgm.info>
2019-10-04 03:59:43 +0900

    pip install yfkをrun.shからdownload.shに移動し た

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 YFK_DIR=$(cd $(dirname $0); pwd)
 
-source $HOME/yfkenv/venv/bin/activate
-pip install --upgrade yfk/proj/dist/yfk-*.*.*-py3-none-any.whl
+# source $HOME/yfkenv/venv/bin/activate
+# pip install --upgrade yfk/proj/dist/yfk-*.*.*-py3-none-any.whl
 
 PY_SCRIPT=$YFK_DIR/run.py
 python $PY_SCRIPT
