9650ee98d0624838fb2f7b0e393ba10b568455a6
PTGM <develop@ptgm.info>
2019-10-05 11:28:58 +0900

    テストデータフィクスチャをセッションスコープに変更した

--- proj/test/test_yahoo_finance_api.py
+++ proj/test/test_yahoo_finance_api.py
@@ -6,13 +6,14 @@ from make_test_data import get_data
 
 OUTPUT_CSV = 'output_csv'
 CODE = 'KO'
+DELTA = 10e-6
 
 @pytest.fixture()
-def ko_monthly_csv():
+def ko_monthly_csv(scope='session'):
     """Yahoo Financeサイトでダウンロードした三種類のCSVファイルを合成したもの"""
     return get_data()
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def ko_from_api():
     """YFinance APIから取得したデータ"""
     ko = YFA(CODE)
@@ -23,28 +24,59 @@ def ko_from_api():
 
 
 def test_open(ko_monthly_csv, ko_from_api):
-    assert ko_monthly_csv.loc['1962-01-01'].loc['Open'] == 0.263021
+    assert ko_monthly_csv.loc['1962-01-01'].loc['Open'] - 0.263021 < DELTA
     assert ko_monthly_csv.loc['1962-01-01'].loc['Open'] -\
-        ko_from_api.loc['1962/01/01'].loc['Open'] < 0.01
+        ko_from_api.loc['1962/01/01'].loc['Open'] < DELTA
+
+
+def test_high(ko_monthly_csv, ko_from_api):
+    assert ko_monthly_csv.loc['1962-01-01'].loc['High'] - 0.270182 < DELTA
+    assert ko_monthly_csv.loc['1962-01-01'].loc['High'] -\
+        ko_from_api.loc['1962/01/01'].loc['High'] < DELTA
+
+
+def test_low(ko_monthly_csv, ko_from_api):
+    assert ko_monthly_csv.loc['1962-01-01'].loc['Low'] - 0.233073 < DELTA
+    assert ko_monthly_csv.loc['1962-01-01'].loc['Low'] -\
+        ko_from_api.loc['1962/01/01'].loc['Low'] < DELTA
+
+
+def test_close(ko_monthly_csv, ko_from_api):
+    assert ko_monthly_csv.loc['1962-01-01'].loc['Close'] - 0.242839 < DELTA
+    assert ko_monthly_csv.loc['1962-01-01'].loc['Close'] -\
+        ko_from_api.loc['1962/01/01'].loc['Close'] < DELTA
+
+
+def test_adj_close(ko_monthly_csv, ko_from_api):
+    assert ko_monthly_csv.loc['1962-01-01'].loc['Adj Close'] - 0.050381 < DELTA
+    assert ko_monthly_csv.loc['1962-01-01'].loc['Adj Close'] -\
+        ko_from_api.loc['1962/01/01'].loc['Adj Close'] < DELTA
+
+
+def test_volume(ko_monthly_csv, ko_from_api):
+    assert ko_monthly_csv.loc['1962-01-01'].loc['Volume'] == 29414400
+    assert ko_monthly_csv.loc['1962-01-01'].loc['Volume'] ==\
+        ko_from_api.loc['1962/01/01'].loc['Volume']
 
 
 def test_dividends(ko_monthly_csv, ko_from_api):
-    ko_from_api.loc['1962/03/01'].loc['Date_Div'] == '1962/03/13'
-    ko_from_api.loc['1962/03/01'].loc['Dividends'] == 0.00156
+    assert ko_from_api.loc['1962/03/01'].loc['Date_Div'] == '1962/03/13'
+    assert ko_from_api.loc['1962/03/01'].loc['Dividends'] - 0.00156 < DELTA
 
-    ko_from_api.loc['1962/03/01'].loc['Date_Div'] == \
-            ko_monthly_csv.loc['1962-03-01'].loc['Date_Div']
+    assert ko_from_api.loc['1962/03/01'].loc['Date_Div'] == \
+            ko_monthly_csv.loc['1962-03-01'].loc['Date_Div'].replace('-', '/')
 
-    ko_from_api.loc['1962/03/01'].loc['Dividends'] == \
-            ko_monthly_csv.loc['1962-03-01'].loc['Dividends']
+    assert ko_from_api.loc['1962/03/01'].loc['Dividends'] == \
+            float(ko_monthly_csv.loc['1962-03-01'].loc['Dividends'])
 
 
 def test_splits(ko_monthly_csv, ko_from_api):
     assert ko_from_api.loc['1965/02/01'].loc['Date_Spli'] == '1965/02/19'
-    assert ko_from_api.loc['1965/02/01'].loc['Stock_Splits'] == 2
+    assert ko_from_api.loc['1965/02/01'].loc['Stock_Splits'] - 2 < DELTA
 
     assert ko_from_api.loc['1965/02/01'].loc['Date_Spli'] == \
             ko_monthly_csv.loc['1965-02-01'].loc['Date_Spli'].replace('-', '/')
 
-    assert ko_from_api.loc['1965/02/01'].loc['Stock_Splits'] == \
-            1 / eval(ko_monthly_csv.loc['1965-02-01'].loc['Stock_Splits'])
+    assert ko_from_api.loc['1965/02/01'].loc['Stock_Splits'] - \
+            1 / eval(ko_monthly_csv.loc['1965-02-01'].loc['Stock_Splits']) \
+            < DELTA
