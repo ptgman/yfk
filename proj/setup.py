d35c51a3427b4db7b43c4c28f19a5087cae7afeb
PTGM <develop@ptgm.info>
2019-10-11 11:14:30 -0400

    yfk-0.2.0

--- proj/setup.py
+++ proj/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yfk',
-    version='0.1.7',
+    version='0.2.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'}
 )
